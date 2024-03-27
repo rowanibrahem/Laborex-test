e4d69c598937aabe817edb9cdc0867a35cd22fc2
octav8us <octav8us@gmail.com>
2024-03-27 14:55:55 +0200

    general fixes -  change some arabic error messages -  add _handleError helper function in remote repo - fix delivery orders cubit

--- lib/data/data source/remote_repo.dart
+++ lib/data/data source/remote_repo.dart
@@ -12,53 +12,56 @@ class RemoteRepo {
   );
   set token(String? value) => token = value;
 
-  Future<String> login(String phoneNumber, String password) async {
+  Future<T> _handleErrors<T>(Future<T> Function() action) async {
     try {
-      final response = await _dio.post(
-        Constants.loginUrl,
-        data: {
-          "phoneNumber": phoneNumber,
-          "password": password,
-        },
-      );
-      if (response.statusCode == 200) {
-        return response.data['token'];
-      } else {
-        throw Exception('Failed to load data');
-      }
+      return await action();
     } catch (error) {
-      throw Exception('Error: $error');
-    }
-  }
-
-  Future<List<DeliverOrderModel>> getOrders(String token) async {
-    try {
-      final response = await _dio.get(
-        Constants.getOrdersUrl,
-        options: Options(
-          headers: {
-            "Authorization": "Bearer $token",
-          },
-        ),
-      );
-
-      if (response.statusCode == 200) {
-        final List<dynamic> data = response.data;
-
-        return data.map((item) => DeliverOrderModel.fromMap(item)).toList();
-      } else {
-        throw Exception('Failed to load data');
-      }
-    } catch (error) {
-if (error is DioException) {
+      if (error is DioException) {
         throw ServerFailure.fromDioError(error);
       } else {
         rethrow;
-      }    }
+      }
+    }
   }
+Future<String> login(String phoneNumber, String password) async {
+  return _handleErrors<String>(() async {
+    final response = await _dio.post(
+      Constants.loginUrl,
+      data: {
+        "phoneNumber": phoneNumber,
+        "password": password,
+      },
+    );
+    if (response.statusCode == 200) {
+      return response.data['token'];
+    } else {
+      throw Exception('Failed to load data');
+    }
+  });
+}
+
+Future<List<DeliverOrderModel>> getOrders(String token) async {
+  return _handleErrors<List<DeliverOrderModel>>(() async {
+    final response = await _dio.get(
+      Constants.getOrdersUrl,
+      options: Options(
+        headers: {
+          "Authorization": "Bearer $token",
+        },
+      ),
+    );
+    if (response.statusCode == 200) {
+      final List<dynamic> data = response.data;
+      return data.map((item) => DeliverOrderModel.fromMap(item)).toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  });
+}
+
 
   Future<String> startDelivery(String token, String orderId) async {
-    try {
+    return _handleErrors<String>(() async {
       final response = await _dio.patch(
         '${Constants.deliveryStartUrl}$orderId',
         options: Options(
@@ -72,14 +75,7 @@ if (error is DioException) {
       } else {
         throw Exception('${response.data}');
       }
-    } catch (error) {
-      if (error is DioException) {
-        throw ServerFailure.fromDioError(error);
-      } else {
-        rethrow;
-      }
-      //throw Exception('Error: $error');
-    }
+    });
   }
 
   Future<String> finishOrder(
@@ -89,7 +85,7 @@ if (error is DioException) {
     String returnType,
     String description,
   ) async {
-    try {
+    return _handleErrors<String>(() async {
       final response = await _dio.post(
         '${Constants.finishOrderUrl}$orderId',
         data: {
@@ -108,12 +104,6 @@ if (error is DioException) {
       } else {
         throw Exception('${response.data}');
       }
-    } catch (error) {
-      if (error is DioException) {
-        throw ServerFailure.fromDioError(error);
-      } else {
-        rethrow;
-      }
-    }
+    });
   }
 }
