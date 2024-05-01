3874a4faed09495e693071ccdf1a5f34b92f4c79
octav8us <octav8us@gmail.com>
2024-05-01 11:48:21 +0300

    changed base_url

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const baseUrl = 'https://dms.ebdaa-business.com/api/v1';
+  static const baseUrl = 'http://203.161.54.164:5152/api/v1';
   static const getOrdersUrl = '$baseUrl/order/driver-orders';
   static const loginUrl = '$baseUrl/auth/login';
   static const deliveryStartUrl = '$baseUrl/order/delivery-start/';
