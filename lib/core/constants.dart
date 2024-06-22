2d1f67293339f51298fd572b398b241dfaf14c48
Rahma <rahmaosama2804@gmail.com>
2024-06-22 10:41:48 +0300

    fix description controller bug

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,9 +1,9 @@
 class Constants {
   //testing server
-  static const baseUrl = 'http://66.29.130.92:5152/api/v1';
+  // static const baseUrl = 'http://66.29.130.92:5152/api/v1';
   //production server
   
-  // static const baseUrl = 'http://203.161.54.164:5152/api/v1';
+  static const baseUrl = 'http://203.161.54.164:5152/api/v1';
   static const getOrdersUrl = '$baseUrl/order/driver-orders';
   static const loginUrl = '$baseUrl/auth/login';
   static const deliveryStartUrl = '$baseUrl/order/delivery-start/';
